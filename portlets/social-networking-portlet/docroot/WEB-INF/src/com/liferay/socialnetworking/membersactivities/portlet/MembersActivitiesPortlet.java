54859fc1e4196efd9912e5a4482719fbf9e61932
Mate Thurzo <mate.thurzo@liferay.com>
2012-01-17 10:21:57 +0100

    LPS-23871 Source formatting

--- portlets/social-networking-portlet/docroot/WEB-INF/src/com/liferay/socialnetworking/membersactivities/portlet/MembersActivitiesPortlet.java
+++ portlets/social-networking-portlet/docroot/WEB-INF/src/com/liferay/socialnetworking/membersactivities/portlet/MembersActivitiesPortlet.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.socialnetworking.membersactivities.portlet;
 
 import com.liferay.portal.kernel.exception.SystemException;
@@ -145,7 +159,7 @@ public class MembersActivitiesPortlet extends MVCPortlet {
 				SocialActivityInterpreterLocalServiceUtil.interpret(
 					activity, themeDisplay);
 
-			if (activityFeedEntry == null) {
+			if (Validator.isNull(activityFeedEntry)) {
 				continue;
 			}
 
