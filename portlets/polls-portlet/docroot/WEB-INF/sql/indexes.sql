a29ac280fc329c6acaf4a074f64719a5f051fbd3
Juan Fernández <juan.fernandez@liferay.com>
2012-10-02 15:54:14 +0200

    LPS-30029 Source Formatting

--- portlets/polls-portlet/docroot/WEB-INF/sql/indexes.sql
+++ portlets/polls-portlet/docroot/WEB-INF/sql/indexes.sql
@@ -10,3 +10,16 @@ create unique index IX_D9C0F36B on Polls_PollsQuestion (uuid_, groupId);
 create index IX_E1609DC9 on Polls_PollsVote (choiceId);
 create index IX_41D37ECE on Polls_PollsVote (questionId);
 create unique index IX_E6442908 on Polls_PollsVote (questionId, userId);
+
+create index IX_9BDCE791 on _PollsChoice (questionId);
+create unique index IX_61558690 on _PollsChoice (questionId, name);
+create index IX_1323CE78 on _PollsChoice (uuid_);
+
+create index IX_ADDFD649 on _PollsQuestion (groupId);
+create index IX_DA586713 on _PollsQuestion (uuid_);
+create index IX_A7DA8335 on _PollsQuestion (uuid_, companyId);
+create unique index IX_992452F7 on _PollsQuestion (uuid_, groupId);
+
+create index IX_C17FBC55 on _PollsVote (choiceId);
+create index IX_96A52A5A on _PollsVote (questionId);
+create unique index IX_A5A78894 on _PollsVote (questionId, userId);
--- portlets/polls-portlet/docroot/WEB-INF/sql/tables.sql
+++ portlets/polls-portlet/docroot/WEB-INF/sql/tables.sql
@@ -32,3 +32,38 @@ create table Polls_PollsVote (
 	choiceId LONG,
 	voteDate DATE null
 );
+
+create table _PollsChoice (
+	uuid_ VARCHAR(75) null,
+	choiceId LONG not null primary key,
+	questionId LONG,
+	name VARCHAR(75) null,
+	description STRING null
+);
+
+create table _PollsQuestion (
+	uuid_ VARCHAR(75) null,
+	questionId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	title STRING null,
+	description STRING null,
+	expirationDate DATE null,
+	lastVoteDate DATE null
+);
+
+create table _PollsVote (
+	voteId LONG not null primary key,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	questionId LONG,
+	choiceId LONG,
+	voteDate DATE null
+);
