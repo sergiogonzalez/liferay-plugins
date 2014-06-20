30ce7e5e0af0413cb9856a200ae3938f0fa682ec
adolfo.perez <adolfo.perez@liferay.com>
2014-06-20 14:31:25 +0200

    LPS-47855 rename all "comments" config. options to "feedback"
    They are not really user comments. Rename them to "feedback" to make clearer its purpose.

---
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/hook/upgrade/v1_4_0/UpgradePortletPreferences.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledgebase.hook.upgrade.v1_4_0;
+
+import com.liferay.portal.kernel.upgrade.BaseUpgradePortletPreferences;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portlet.PortletPreferencesFactoryUtil;
+
+import javax.portlet.PortletPreferences;
+import javax.portlet.ReadOnlyException;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class UpgradePortletPreferences extends BaseUpgradePortletPreferences {
+
+	@Override
+	protected String[] getPortletIds() {
+		return _PORTLET_IDS;
+	}
+
+	protected void renamePreference(
+			PortletPreferences portletPreferences, String oldName,
+			String newName)
+		throws ReadOnlyException {
+
+		boolean value = GetterUtil.getBoolean(
+			portletPreferences.getValue(oldName, null));
+
+		portletPreferences.setValue(newName, Boolean.toString(value));
+
+		portletPreferences.reset(oldName);
+	}
+
+	@Override
+	protected String upgradePreferences(
+			long companyId, long ownerId, int ownerType, long plid,
+			String portletId, String xml)
+		throws Exception {
+
+		PortletPreferences portletPreferences =
+			PortletPreferencesFactoryUtil.fromXML(
+				companyId, ownerId, ownerType, plid, portletId, xml);
+
+		renamePreference(
+			portletPreferences, "enableKBArticleKBComments",
+			"enableKBArticleFeedback");
+
+		renamePreference(
+			portletPreferences, "showKBArticleKBComments",
+			"showKBArticleFeedback");
+
+		return PortletPreferencesFactoryUtil.toXML(portletPreferences);
+	}
+
+	private static final String[] _PORTLET_IDS = new String[] {
+		"2_WAR_knowledgebaseportlet", "3_WAR_knowledgebaseportlet_INSTANCE_%",
+		"4_WAR_knowledgebaseportlet_INSTANCE_%", "5_WAR_knowledgebaseportlet"
+	};
+
+}
