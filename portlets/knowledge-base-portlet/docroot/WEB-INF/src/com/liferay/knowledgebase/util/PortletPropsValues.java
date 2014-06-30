a3559ff8be4fab7f0006172a6440d447c3983f48
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2014-06-30 10:16:24 +0200

    LPS-46840 Remove hardcoded values and move them to portlet.properties

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/util/PortletPropsValues.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/util/PortletPropsValues.java
@@ -63,8 +63,29 @@ public class PortletPropsValues {
 	public static final String[] ADMIN_KB_ARTICLE_SECTIONS =
 		PortletProps.getArray(PortletPropsKeys.ADMIN_KB_ARTICLE_SECTIONS);
 
+	public static final String[] MARKDOWN_IMPORTER_ARTICLE_EXTENSIONS =
+		PortletProps.getArray(
+			PortletPropsKeys.MARKDOWN_IMPORTER_ARTICLE_EXTENSIONS);
+
+	public static final String MARKDOWN_IMPORTER_ARTICLE_HOME =
+		GetterUtil.getString(
+			PortletProps.get(PortletPropsKeys.MARKDOWN_IMPORTER_ARTICLE_HOME));
+
+	public static final String MARKDOWN_IMPORTER_ARTICLE_INTRO =
+		GetterUtil.getString(
+			PortletProps.get(PortletPropsKeys.MARKDOWN_IMPORTER_ARTICLE_INTRO));
+
+	public static final String MARKDOWN_IMPORTER_ARTICLE_INTRODUCTION =
+		GetterUtil.getString(
+			PortletProps.get(
+				PortletPropsKeys.MARKDOWN_IMPORTER_ARTICLE_INTRODUCTION));
+
 	public static final String[] MARKDOWN_IMPORTER_IMAGE_FILE_EXTENSIONS =
 		PortletProps.getArray(
 			PortletPropsKeys.MARKDOWN_IMPORTER_IMAGE_FILE_EXTENSIONS);
+
+	public static final String MARKDOWN_IMPORTER_IMAGE_FOLDER =
+		GetterUtil.getString(
+			PortletProps.get(PortletPropsKeys.MARKDOWN_IMPORTER_IMAGE_FOLDER));
 
 }
