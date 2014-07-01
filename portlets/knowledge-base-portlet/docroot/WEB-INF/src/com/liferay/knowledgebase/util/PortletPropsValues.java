bf98d6d9768d38c364b7adecff7777a32dd4f6a5
adolfo.perez <adolfo.perez@liferay.com>
2014-07-01 10:15:43 +0200

    LPS-48146 use configured number of stars in ratings

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/util/PortletPropsValues.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/util/PortletPropsValues.java
@@ -15,6 +15,8 @@
 package com.liferay.knowledgebase.util;
 
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.util.portlet.PortletProps;
 
 /**
@@ -68,5 +70,9 @@ public class PortletPropsValues {
 			PortletProps.get(
 				PortletPropsKeys.
 					KNOWLEDGE_BASE_RATINGS_DEFAULT_NUMBER_OF_STARS));
+
+	public static final int PORTAL_RATINGS_DEFAULT_NUMBER_OF_STARS =
+		GetterUtil.getInteger(
+			PropsUtil.get(PropsKeys.RATINGS_DEFAULT_NUMBER_OF_STARS));
 
 }
